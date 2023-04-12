476513384aa2a652209debebcfa3907728752084
Evgenija2000 <82340818+Evgenija2000@users.noreply.github.com>
2023-04-12 16:57:18 +0200

    updates

--- style/script.js
+++ style/script.js
@@ -1,7 +1,10 @@
-const scrollUp = document.querySelector('#scroll-up');
+//hamburger meni
 const burger = document.querySelector('#burger-menu');
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
+
+const scrollUp = document.querySelector('#scroll-up');
+
 const navLink = document.querySelectorAll(".nav-link");
 
 burger.addEventListener("click", () => {
@@ -13,7 +16,7 @@ navLink.forEach((link) =>
         ul.classList.remove("show");
     })
 );
-scrollUp.addEventListener("click",() => {
+scrollUp.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         left: 0,
